f96b58408e79b944cbcdeebe36a68b9db7bf5b77
Abhishek-143-op <ab8188036@gmail.com>
2025-08-02 23:21:16 +0530

    Multilines in a preserved format

---
+++ Basic_of_python/B-python-2.py
@@ -0,0 +1,6 @@
+# Learning to print in a preserved format 
+print ("""Hello , this is 2nd python code. 
+and here I am learning to print things like 
+text , string , varible etc.. (multi-lines)  
+       
+     IN A PRESERVED FORMAT      """)

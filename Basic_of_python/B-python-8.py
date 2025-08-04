868d2e48f40e01ea536c188ab908b4ccad09d73c
Abhishek-143-op <ab8188036@gmail.com>
2025-08-05 00:15:42 +0530

    Learning about the strings and related terms

---
+++ Basic_of_python/B-python-8.py
@@ -0,0 +1,19 @@
+                         #Learning about the strings.
+
+print("""
+String: It is datatype in python and other coding language .
+            Which actually is sequence of characters in a quotes ("____"or '____' or """ """)
+Strings are primarilly written in 3 ways :
+\n 
+1.  Inside : single quotes '_____'        -----   need \ for using '' inside string 
+2.  Inside : double quotes "_____"        -----   normal 
+3.  Inside : triple quotes """"___"""""   -----   multilines strings 
+\n
+""")
+# NOTE :There is no difference between the all these 3 but In the single quotes if we want single quotes in string inside single quoted string then must use \ as shown above 
+
+somestring= 'That\'s string ' # here we use \ to use ' inside the string 
+
+
+#---------------------------------------STRING SLICING --------------------------------------------
+

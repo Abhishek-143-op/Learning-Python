a718faa2e8930d92e07c134eaf06eefd60732781
Abhishek-143-op <ab8188036@gmail.com>
2025-08-04 22:41:26 +0530

    Copying teacher-- 1st python code

--- Basic_of_python/B-python-1.py
+++ Basic_of_python/B-python-1.py
@@ -1,5 +1,6 @@
 import pyjokes
 #copying the teachers directly as I don't know anything about the python
 pyjokes = pyjokes.get_joke()
-print("Hello, world!  ")
+print("Hello, world! My name is Abhishek. and I am trying to learn the python ")
 print(pyjokes)
+

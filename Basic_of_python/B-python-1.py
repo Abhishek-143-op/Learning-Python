b405e7d19d119143b18cd725cd359f59bb42dfac
Abhishek-143-op <ab8188036@gmail.com>
2025-08-05 20:40:29 +0530

    Copying teacher , first code and comments

--- Basic_of_python/B-python-1.py
+++ Basic_of_python/B-python-1.py
@@ -4,3 +4,16 @@ pyjokes = pyjokes.get_joke()
 print("Hello, world! My name is Abhishek. and I am trying to learn the python ")
 print(pyjokes)
 
+#Learning about the comments
+
+"""
+comments are written line in the code for our convinence to make sourse side code more readable which further 
+helps in creating efficient code both for team work and Solo .
+
+every things written towards right after the # is comment.
+and 
+every things written under 3 quotes """"" """" is also an comment.
+
+"""
+
+

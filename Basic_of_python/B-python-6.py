78bda7627ba52295ca2d3986ed96dbff7f8ab832
Abhishek-143-op <ab8188036@gmail.com>
2025-08-07 01:03:48 +0530

    Learning about the input function and special strings -- f and r string

--- Basic_of_python/B-python-6.py
+++ Basic_of_python/B-python-6.py
@@ -18,11 +18,30 @@ name = input ("Enter your name : ")  # Here we donot define the datatype so by d
 
 print ("Welcome ,", name )
 
-age = int (input ("Enter your",name,"age : ")) # Here input dataype is defined so it will take input as integer.
+age = int (input (f"{name},Enter your age : ")) # Here input dataype is defined so it will take input as integer.
 
 if age>18:
  print(name, "You are an adult ")
 else:
  print(name, "your are not an adult ")
   
-  
+  # OTHERE WAY OF INPUTING THE VARIABLE IN THE STRING :
+
+print(f"Hello, dear ,{name}")
+"""                 |----|
+                     |
+                     here we directly use the variable in the string   
+
+          """
+
+print(
+r"""NOTE : 
+1.  fstring : fstring is used to embeded the variable , function , class inside the strings 
+                     synatax :
+                               f"some text {expression}" #placing the f in front of the string 
+2. rstring: rstring is used to print the raw Whatever written iside of it . It ignores embeded varible, funtion , class, and escaping 
+             sequence like \n , \t etc. 
+
+3. If i find more will be updated . 
+
+""")

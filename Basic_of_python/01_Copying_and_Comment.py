ccd969a2dbcdc661a9e9d263a4ee05cee3dd8c77
Abhishek-143-op <ab8188036@gmail.com>
2025-08-10 18:03:12 +0530

    Copying and Learning about comments , Modules , PIP , First pyhton code

--- Basic_of_python/01_Copying_and_Comment.py
+++ Basic_of_python/01_Copying_and_Comment.py
@@ -15,5 +15,14 @@ and
 every things written under 3 quotes """"" """" is also an comment.
 
 """
-
+print("""*************************************Modules and PIP *******************************************************
+      
+      Modules: A file containing Someone else code which directly can be used in our code without any copyright issues
+                It is of two types 
+                   |---(a). Built in modules : Preinstalled with python. Ex: import os, random etc.
+                   |---(b). External module  : Need to install using the PIP (python interpreter program )
+                                                  Ex. 1. pip install flask (in the terminal)
+                                                      2. pip install django (in the terminal) 
+                                                      3. pip install tensorflow (in the terminal)    
+      """)
 

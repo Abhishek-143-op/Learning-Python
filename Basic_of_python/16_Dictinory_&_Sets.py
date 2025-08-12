4f6b34deee669ca57cc545c6ea74c6c050e7f044
Abhishek-143-op <ab8188036@gmail.com>
2025-08-12 23:47:13 +0530

    Learning about dictinory and its functions

---
+++ Basic_of_python/16_Dictinory_&_Sets.py
@@ -0,0 +1,26 @@
+            #Here I am learning About Dictinory and Sets.
+
+print(
+"""
+Dictinory is the set of the key0-Value pair .
+
+as shown:
+
+""")
+dictinory1= {
+            "Name":"Abhishek",
+            "Age":"18",
+            "Roll no.":"341123" ,
+            }
+
+#  Using the dictionary functions 
+print(
+dictinory1.get("Age"),
+dictinory1.keys(),
+dictinory1.pop("Age"),
+dictinory1.update({"Marks":"[23,54,12]"}),
+dictinory1.setdefault(("name","John")),
+dictinory1.values(),
+dictinory1.items(),
+
+)

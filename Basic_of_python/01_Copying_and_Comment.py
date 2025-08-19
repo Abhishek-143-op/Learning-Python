dae17af8bab9e5580ce494a2e002afe0fa9da103
Abhishek-143-op <ab8188036@gmail.com>
2025-08-19 22:58:03 +0530

    Improving readibility by rearraging lines

--- Basic_of_python/01_Copying_and_Comment.py
+++ Basic_of_python/01_Copying_and_Comment.py
@@ -1,5 +1,7 @@
-import pyjokes
 #copying the teachers directly as I don't know anything about the python
+
+
+import pyjokes
 pyjokes = pyjokes.get_joke()
 print("Hello, world! My name is Abhishek. and I am trying to learn the python ")
 print(pyjokes)

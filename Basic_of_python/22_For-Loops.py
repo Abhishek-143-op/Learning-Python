13822c6c125541697613de4a57cef237aabf275f
Abhishek-143-op <ab8188036@gmail.com>
2025-08-26 00:50:21 +0530

    Improving readiblity of the code/notes

--- Basic_of_python/22_For-Loops.py
+++ Basic_of_python/22_For-Loops.py
@@ -1,9 +1,6 @@
 #                                       Basic of  for loops.
-
-
 print("""
                 Learning about the loops :
-      
       Loops are required because we want certain line code to be executed untill the condition is true .
        |
        |--- Loops are 2 type in python.
@@ -11,13 +8,13 @@ print("""
                 |--- 1. for loop.
                 |       
                 |--- 2. while loop.
-
 """)
 
+
+
+
 print("If we want to print 1 to 100 ")
-
 for i in range (1, 101): 
-
     print (i) # --, --
               #  |   |                              Here concept of step is also there in the loop just like string.
               # start , end 
@@ -25,15 +22,23 @@ for i in range (1,10,2):
     print ("with the step we can print only odd no. in 1 - 10:",i)
 
 
+
+
 #print from the lists 
 fruits= ["Apple", "Mango", "pineapple", "papaya "]
 for i in fruits:
     print(i)
 
+
+
+
 #print from the tuple
 tuple=(1,2,3,5,4)
 for i in tuple:
     print(i)
+
+
+
 
 
 #print only keys From the dictinary
@@ -43,14 +48,33 @@ dictinary1 = {
 }
 for i in dictinary1 :
     print ("Here only keys will be printed :" ,i)
-
 #For printing both keys and value from the dictinary.
-
 for i ,j in dictinary1.items() :
     print ("Here both keys and values will be printed :" ,i,"--->",j)
 
+
+
 #similarly in the we can print items from the sets .
+
+
+
 
 for ch in "Python":
     print(ch) 
 # Here all the character will be printed in the string = "python "-- here.
+
+
+
+
+
+
+
+
+
+#******************************************NOTE*********************************************************
+"""
+If we are using :
+                
+"""
+for i in range (5): # Here starting will be done from the 0-4 Like : 0,1,2,3,4. 
+    print(i,end=" ")

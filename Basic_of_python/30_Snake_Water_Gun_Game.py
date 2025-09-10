0ee8bf63950da8a8505205258f1685e8459bd107
Abhishek-143-op <ab8188036@gmail.com>
2025-09-10 21:41:29 +0530

    Adding the more info about the pattern in game or code

--- Basic_of_python/30_Snake_Water_Gun_Game.py
+++ Basic_of_python/30_Snake_Water_Gun_Game.py
@@ -37,3 +37,59 @@ elif comchoice == 'g' and userchoice == 's':
 # Invalid input
 else:
     print("Invalid input! Please choose S, W, or G.")
+
+
+
+#-----------------------------------------------------OR--------------------------------------------------------------
+
+
+"""
+In such games and codes sometime we create patterns like here for wining and losing : due to which we 
+can replace multiple lines of codes with very few lines which make code simpler but complex to the(USER-who codes) new person 
+completely unfamilar with code . 
+
+Which can be efficient in some cases (Here in this code there is no pattern as code is written in well defined 
+any user understand able way . :} )
+
+"""
+computer = random.choice([-1, 0, 1])
+youstr = input("Enter your choice: ")
+youDict = {"s": 1, "w": -1, "g": 0}
+reverseDict = {1: "Snake", -1: "Water", 0: "Gun"}
+
+you = youDict[youstr]
+
+# By now we have 2 numbers (variables), you and computer
+
+print(f"You chose {reverseDict[you]}\nComputer chose {reverseDict[computer]}")
+
+if(computer == you):
+    print("Its a draw")
+
+else:
+    '''
+     if(computer ==-1 and you == 1): (computer - you) = -2
+        print("You win!")
+
+    elif(computer ==-1 and you == 0): (computer - you) = -1
+        print("You Lose!")
+
+    elif(computer == 1 and you == -1): (computer - you) = 2
+        print("You lose!")
+
+    elif(computer ==1 and you == 0): (computer - you) = 1
+        print("You Win!")
+
+    elif(computer ==0 and you == -1): (computer - you) = 1
+        print("You Win!")
+
+    elif(computer == 0 and you == 1): computer - you) = -1
+        print("You Lose!") 
+
+        The below logic is written on the basis of the value of computer - you
+ 
+    '''
+    if((computer - you) == -1 or  (computer - you) == 2 ):
+        print("You lose!")
+    else:
+        print("You win!") 

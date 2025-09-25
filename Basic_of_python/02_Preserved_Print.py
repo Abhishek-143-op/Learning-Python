663f05e08e08a23d37e642c8494cb5a065a6704f
Abhishek-143-op <ab8188036@gmail.com>
2025-09-25 19:27:24 +0530

    Stings can be given to the variable -- shown with examplar code

--- Basic_of_python/02_Preserved_Print.py
+++ Basic_of_python/02_Preserved_Print.py
@@ -4,3 +4,10 @@ and here I am learning to print things like
 text , string , varible etc.. (multi-lines)  
        
      IN A PRESERVED FORMAT      """)
+
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  This also can be done @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@2222
+
+variable = """\n \n  Hello, this is wnd python code. and here I am learning to print 
+                         like text, string, variable, etc... (multi-lines)"""
+print (variable)
